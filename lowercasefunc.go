61dacada78fc2d278f576d9d8255d35dec69bb05
shibh308 <s17097@tokyo.kosen-ac.jp>
2020-09-03 17:32:10 +0900

    [update]change FuncObj to LowerDecl

--- lowercasefunc.go
+++ lowercasefunc.go
@@ -5,11 +5,10 @@ import (
 	"go/ast"
 	"go/token"
 	"go/types"
-	"path/filepath"
-	"strings"
-
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
+	"path/filepath"
+	"strings"
 )
 
 const doc = "lowercasefunc is ..."
@@ -42,77 +41,107 @@ func getFuncObj(pkg *types.Package, name string) types.Object {
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	var result []LowerCaseFunc
+
+	exportedFunc := make(map[string]*ast.FuncDecl)
+	unexportedFunc := make(map[string]*ast.FuncDecl)
+
 	for _, f := range pass.Files {
 		for _, decl := range f.Decls {
 			fd, ok := decl.(*ast.FuncDecl)
-			if !ok || !fd.Name.IsExported() {
+			if !ok {
 				continue
 			}
 			name := fd.Name.String()
-			lower := strings.ToLower(string(name[0])) + name[1:]
-			target := getFuncObj(pass.Pkg, lower)
-			if target == nil {
-				continue
+			if fd.Name.IsExported() {
+				exportedFunc[name] = fd
+			} else {
+				unexportedFunc[name] = fd
 			}
-			body := fd.Body
-			var calledPos []token.Pos
-			ast.Inspect(body, func(n ast.Node) bool {
-				id, ok := n.(*ast.Ident)
-				if ok && pass.TypesInfo.Uses[id] == target {
-					calledPos = append(calledPos, id.Pos())
-				}
-				return true
-			})
-			p := pass.Fset.Position(target.Pos())
-			s := fmt.Sprintf("{FuncName:%s, TargetPos:\"%s:%d:%d\", CalledPos:[", fd.Name.String(), filepath.Base(p.Filename), p.Column, p.Line)
-			var cpStr []string
-			for _, cp := range calledPos {
-				p := pass.Fset.Position(cp)
-				cpStr = append(cpStr, fmt.Sprintf("\"%s:%d:%d\"", filepath.Base(p.Filename), p.Column, p.Line))
-			}
-			pass.Reportf(fd.Pos(), s + strings.Join(cpStr, ", ") + "]}\n")
 		}
 	}
-
+	for name, fd := range exportedFunc {
+		lower := strings.ToLower(string(name[0])) + name[1:]
+		ufd, ok := unexportedFunc[lower]
+		if !ok {
+			continue
+		}
+		target := pass.TypesInfo.Defs[ufd.Name]
+		body := fd.Body
+		var calledPos []token.Pos
+		ast.Inspect(body, func(n ast.Node) bool {
+			id, ok := n.(*ast.Ident)
+			if ok && pass.TypesInfo.Uses[id] == target {
+				calledPos = append(calledPos, id.Pos())
+			}
+			return true
+		})
+		p := pass.Fset.Position(target.Pos())
+		s := fmt.Sprintf("{FuncName:%s, TargetPos:\"%s:%d:%d\", CalledPos:[", fd.Name.String(), filepath.Base(p.Filename), p.Column, p.Line)
+		var cpStr []string
+		for _, cp := range calledPos {
+			p := pass.Fset.Position(cp)
+			cpStr = append(cpStr, fmt.Sprintf("\"%s:%d:%d\"", filepath.Base(p.Filename), p.Column, p.Line))
+		}
+		pass.Reportf(fd.Pos(), s + strings.Join(cpStr, ", ") + "]}\n")
+		result = append(result,
+			LowerCaseFunc{
+				UpperDecl: fd,
+				LowerDecl: ufd,
+				CalledPos: calledPos,
+			})
+	}
 	return nil, nil
 }
 
 type LowerCaseFunc struct{
 	UpperDecl    *ast.FuncDecl
-	LowerObj     types.Object
+	LowerDecl    *ast.FuncDecl
 	CalledPos     []token.Pos
 }
 
 func runDetect(pass *analysis.Pass) (interface{}, error) {
 	var result []LowerCaseFunc
+
+	exportedFunc := make(map[string]*ast.FuncDecl)
+	unexportedFunc := make(map[string]*ast.FuncDecl)
+
 	for _, f := range pass.Files {
 		for _, decl := range f.Decls {
 			fd, ok := decl.(*ast.FuncDecl)
-			if !ok || !fd.Name.IsExported() {
+			if !ok {
 				continue
 			}
 			name := fd.Name.String()
-			lower := strings.ToLower(string(name[0])) + name[1:]
-			target := getFuncObj(pass.Pkg, lower)
-			if target == nil {
-				continue
+			if fd.Name.IsExported() {
+				exportedFunc[name] = fd
+			} else {
+				unexportedFunc[name] = fd
 			}
-			body := fd.Body
-			var calledPos []token.Pos
-			ast.Inspect(body, func(n ast.Node) bool {
-				id, ok := n.(*ast.Ident)
-				if ok && pass.TypesInfo.Uses[id] == target {
-					calledPos = append(calledPos, id.Pos())
-				}
-				return true
-			})
-			result = append(result,
-				LowerCaseFunc{
-					UpperDecl: fd,
-					LowerObj: target,
-					CalledPos: calledPos,
-				})
 		}
+	}
+	for name, fd := range exportedFunc {
+		lower := strings.ToLower(string(name[0])) + name[1:]
+		ufd, ok := unexportedFunc[lower]
+		if !ok {
+			continue
+		}
+		target := pass.TypesInfo.Defs[ufd.Name]
+		body := fd.Body
+		var calledPos []token.Pos
+		ast.Inspect(body, func(n ast.Node) bool {
+			id, ok := n.(*ast.Ident)
+			if ok && pass.TypesInfo.Uses[id] == target {
+				calledPos = append(calledPos, id.Pos())
+			}
+			return true
+		})
+		result = append(result,
+			LowerCaseFunc{
+				UpperDecl: fd,
+				LowerDecl: ufd,
+				CalledPos: calledPos,
+			})
 	}
 
 	return result, nil
